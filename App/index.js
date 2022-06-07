21be562b5667cd0c5d81821970cb56f1b4f6134e
E-Waste <ewaste@E-Wastes-MacBook-Pro.local>
2022-06-06 18:17:52 -0700

    Working on making sure all my data shows up on the the page

--- App/index.js
+++ App/index.js
@@ -1,8 +1,16 @@
+/*  NEED TO INSTALL DOTENV
+if(process.env.NODE_ENV != "production"){
+    require("dotenv").config();
+}
+*/
+
 // Dependencies here
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
-const res = require("express/lib/response");
+const ejsMate = require("ejs-mate");
+const methodOverride = require("method-override");
+const portfolioData = require("./Public/Data/portfolioData.json")
 
 //Connect to database
 mongoose.connect("mongodb://localhost:27017/YelpCamp", {
@@ -20,10 +28,23 @@ db.once("open", () => {
 // Initialize application
 const app = express();
 
+//Middleware
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "Views"));
+app.use(express.static(path.join(__dirname, "Public")));
+
+
 /***************************************************
-GET Request for homepage */
+GET Request for Homepage */
 app.get("/", (req, res) => {
-    res.send("HELLO");
+    res.render("Portfolio/index", {portfolioData});
+})
+
+/***************************************************
+GET Request for Resume */
+app.get("/resume", (req, res) => {
+    res.render("Portfolio/resume", {portfolioData});
 })
     
 
